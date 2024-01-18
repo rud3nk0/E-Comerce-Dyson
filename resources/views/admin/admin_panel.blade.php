53bf89dc283480c4ed2578e69e513de4bae881dd
rud3nk0 <130237226+rud3nk0@users.noreply.github.com>
2024-01-18 19:37:52 +0200

    Create admin_panel.blade.php

---
+++ resources/views/admin/admin_panel.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Product</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+@endsection
